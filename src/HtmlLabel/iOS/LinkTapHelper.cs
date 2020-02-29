c40f9ba9fc0bdcd93600492f15fa2977a4e2fc0e
Matteo Bortolazzo <matteobortolazzo@pm.me>
2020-02-29 18:50:05 +0000

    Refactors iOS Renderer.

---
+++ src/HtmlLabel/iOS/LinkTapHelper.cs
@@ -0,0 +1,91 @@
+﻿using CoreGraphics;
+using Foundation;
+using LabelHtml.Forms.Plugin.Abstractions;
+using System;
+using UIKit;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace LabelHtml.Forms.Plugin.iOS
+{
+	internal static class UiLabelExtensions
+    {
+        public static void LinkTapHelper(this UILabel control, HtmlLabel element)
+		{
+			void TapHandler(UITapGestureRecognizer tap)
+			{
+				var detectedUrl = DetectTappedUrl(tap, (UILabel)tap.View);
+				if (detectedUrl != null)
+				{
+					var args = new WebNavigatingEventArgs(WebNavigationEvent.NewPage, new UrlWebViewSource { Url = detectedUrl }, detectedUrl);
+					element.SendNavigating(args);
+
+					if (!args.Cancel)
+					{
+						Launcher.OpenAsync(new Uri(detectedUrl)).GetAwaiter().GetResult();
+						element.SendNavigated(args);
+					}
+				}							
+			}
+
+			var tapGesture = new UITapGestureRecognizer(TapHandler);
+			control.AddGestureRecognizer(tapGesture);
+			control.UserInteractionEnabled = true;
+		}
+
+		private static string DetectTappedUrl(UIGestureRecognizer tap, UILabel control)
+		{
+			CGRect bounds = control.Bounds;
+			NSAttributedString attributedText = control.AttributedText;
+
+			// Setup containers
+			using var textContainer = new NSTextContainer(bounds.Size)
+			{
+				LineFragmentPadding = 0,
+				LineBreakMode = control.LineBreakMode,
+				MaximumNumberOfLines = (nuint)control.Lines
+			};
+
+			using var layoutManager = new NSLayoutManager();
+			layoutManager.AddTextContainer(textContainer);
+
+			using var textStorage = new NSTextStorage();
+			textStorage.SetString(attributedText);
+
+			using var fontAttributeName = new NSString("Font");
+			var textRange = new NSRange(0, control.AttributedText.Length);
+			textStorage.AddAttribute(fontAttributeName, control.Font, textRange);
+			textStorage.AddLayoutManager(layoutManager);	
+			CGRect textBoundingBox = layoutManager.GetUsedRectForTextContainer(textContainer);
+
+			// Calculate align offset
+			static nfloat GetAlignOffset(UITextAlignment textAlignment) => textAlignment switch
+				{
+					UITextAlignment.Center => 0.5f,
+					UITextAlignment.Right => 1f,
+					_ => 0.0f,
+				};
+			nfloat alignmentOffset = GetAlignOffset(control.TextAlignment);
+			nfloat xOffset = (bounds.Size.Width - textBoundingBox.Size.Width) * alignmentOffset - textBoundingBox.Location.X;
+			nfloat yOffset = (bounds.Size.Height - textBoundingBox.Size.Height) * alignmentOffset - textBoundingBox.Location.Y;
+
+			// Find tapped character
+			CGPoint locationOfTouchInLabel = tap.LocationInView(control);
+			var locationOfTouchInTextContainer = new CGPoint(locationOfTouchInLabel .X - xOffset, locationOfTouchInLabel .Y - yOffset);
+			var characterIndex = (nint)layoutManager.GetCharacterIndex(locationOfTouchInTextContainer, textContainer);
+			var lineTapped = ((int)Math.Ceiling(locationOfTouchInLabel.Y / control.Font.LineHeight)) - 1;
+			var rightMostPointInLineTapped = new CGPoint(bounds.Size.Width, control.Font.LineHeight * lineTapped);
+			var charsInLineTapped = (nint)layoutManager.GetCharacterIndex(rightMostPointInLineTapped, textContainer);
+
+			if (characterIndex < charsInLineTapped)
+			{
+				return null;
+			}
+
+			// Try to get the URL
+			using var linkAttributeName = new NSString("Link");
+			NSObject linkAttributeValue = attributedText.GetAttribute(linkAttributeName, characterIndex, out NSRange range);
+			return linkAttributeValue is NSUrl url ? url.AbsoluteString : null;
+		}
+	}
+}
--- src/HtmlLabel/iOS/Renderer.cs
+++ src/HtmlLabel/iOS/Renderer.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 using System.ComponentModel;
 using Foundation;
-using CoreGraphics;
 using LabelHtml.Forms.Plugin.Abstractions;
 using LabelHtml.Forms.Plugin.iOS;
 using UIKit;
-using Xamarin.Essentials;
 
 [assembly: ExportRenderer(typeof(HtmlLabel), typeof(HtmlLabelRenderer))]
 namespace LabelHtml.Forms.Plugin.iOS
@@ -55,163 +51,29 @@ namespace LabelHtml.Forms.Plugin.iOS
 				SetText(Control, styledHtml);
 				SetNeedsDisplay();
 			}
-		}
+		}		
 
 		private void SetText(UILabel control, string html)
 		{
-			var attr = new NSAttributedStringDocumentAttributes();
-			var nsError = new NSError();
-			attr.DocumentType = NSDocumentType.HTML;			
-			
-			var htmlData = NSData.FromString(html, NSStringEncoding.Unicode);
-
-			var attributedString = new NSAttributedString(htmlData, attr, ref nsError);
-			var mutable = new NSMutableAttributedString();
-
-			using (var newLine = new NSString("\n"))
+			// Create HTML data sting
+			var stringType = new NSAttributedStringDocumentAttributes
 			{
-				if (mutable.MutableString.HasSuffix(newLine))
-				{
-					mutable.DeleteRange(new NSRange(mutable.MutableString.Length - 1, 1));
-				}
-			}			
-
-			var links = new List<LinkData>();
-			control.AttributedText = mutable;
-
-			// Makes a list of all links:
-			mutable.EnumerateAttributes(new NSRange(0, mutable.Length), NSAttributedStringEnumeration.LongestEffectiveRangeNotRequired, (NSDictionary attrs, NSRange range, ref bool stop) =>
-			{
-				foreach (KeyValuePair<NSObject, NSObject> a in attrs) // should use attrs.ContainsKey(something) instead
-				{
-					if (a.Key.ToString() != "NSLink")
-                    {
-                        continue;
-                    }
-
-                    links.Add(new LinkData(range, a.Value.ToString()));
-					return;
-				}
-			});
-
-			// Sets up a Gesture recognizer:
-			if (links.Count <= 0)
-            {
-                return;
-            }
-
-            control.UserInteractionEnabled = true;
-
-			void TapLinkAction(UITapGestureRecognizer tap)
-			{
-				var url = DetectTappedUrl(tap, (UILabel)tap.View, links);
-				if (url == null)
-				{
-					return;
-				}
-
-				var label = (HtmlLabel)Element;
-				var args = new WebNavigatingEventArgs(WebNavigationEvent.NewPage, new UrlWebViewSource { Url = url }, url);
-				label.SendNavigating(args);
-
-				if (args.Cancel)
-				{
-					return;
-				}
-
-				Launcher.OpenAsync(new Uri(url)).GetAwaiter().GetResult();
-				label.SendNavigated(args);
-			}
-
-			var tapGesture = new UITapGestureRecognizer(TapLinkAction);
-			control.AddGestureRecognizer(tapGesture);
-		}
-
-		private string DetectTappedUrl(UIGestureRecognizer tap, UILabel label, IEnumerable<LinkData> linkList)
-		{
-			// Creates instances of NSLayoutManager, NSTextContainer and NSTextStorage
-			using var layoutManager = new NSLayoutManager();
-			using var textContainer = new NSTextContainer();
-			using var textStorage = new NSTextStorage();
-			textStorage.SetString(label.AttributedText);
-
-			// Configures layoutManager and textStorage
-			layoutManager.AddTextContainer(textContainer);
-			textStorage.AddLayoutManager(layoutManager);
-
-			// Configures textContainer
-			textContainer.LineFragmentPadding = 0;
-			textContainer.LineBreakMode = label.LineBreakMode;
-			textContainer.MaximumNumberOfLines = (nuint)label.Lines;
-            CGSize labelSize = label.Bounds.Size;
-			textContainer.Size = labelSize;
-
-            // Finds the tapped character location and compare it to the specified range
-            CGPoint locationOfTouchInLabel = tap.LocationInView(label);
-            CGRect textBoundingBox = layoutManager.GetUsedRectForTextContainer(textContainer);
-			var textContainerOffset = new CGPoint((labelSize.Width - textBoundingBox.Size.Width) * 0.0 - textBoundingBox.Location.X,
-				(labelSize.Height - textBoundingBox.Size.Height) * 0.0 - textBoundingBox.Location.Y);
-
-			nfloat labelX = Element.HorizontalTextAlignment switch
-			{
-				TextAlignment.End => locationOfTouchInLabel.X - (labelSize.Width - textBoundingBox.Size.Width),
-				TextAlignment.Center => locationOfTouchInLabel.X - (labelSize.Width - textBoundingBox.Size.Width) / 2,
-				_ => locationOfTouchInLabel.X,
+				DocumentType = NSDocumentType.HTML
 			};
-			var locationOfTouchInTextContainer = new CGPoint(labelX - textContainerOffset.X, locationOfTouchInLabel.Y - textContainerOffset.Y);
-
-            var indexOfCharacter = (nint)layoutManager.GetCharacterIndex(locationOfTouchInTextContainer, textContainer);
-
-			nint scaledIndexOfCharacter = 0;
-			// Problem is that method CharacterIndexForPoint always returns index based on UILabel font
-			// ".SFUIText" which is the default Helvetica iOS font
-			// HACK is to scale indexOfCharacter for 13% because NeoSans-Light is narrower font than ".SFUIText"
-			if (label.Font.Name == "NeoSans-Light")
+			var nsError = new NSError();
+			var htmlData = NSData.FromString(html, NSStringEncoding.Unicode);
+			var htmlString = new NSAttributedString(htmlData, stringType, ref nsError);
+			var mutableHtmlString = new NSMutableAttributedString(htmlString);
+			
+			using var newLine = new NSString("\n");
+			if (mutableHtmlString.MutableString.HasSuffix(newLine))
 			{
-				scaledIndexOfCharacter = (nint)(indexOfCharacter * 1.13);
+				mutableHtmlString.DeleteRange(new NSRange(mutableHtmlString.MutableString.Length - 1, 1));
 			}
 
-			// HelveticaNeue font family works perfect until character position in the string is more than 2000 chars
-			// some uncosnsistent behaviour
-			// if string has <b> tag than label.Font.Name from HelveticaNeue-Thin goes to HelveticaNeue-Bold
-			if (label.Font.Name.StartsWith("HelveticaNeue", StringComparison.InvariantCulture))
-			{
-				scaledIndexOfCharacter = (nint)(indexOfCharacter * 1.02);
-			}
-
-			foreach (LinkData link in linkList)
-			{
-                nint rangeLength = link.Range.Length;
-				var tolerance = 0;
-				if (label.Font.Name == "NeoSans-Light")
-				{
-					rangeLength = (nint)(rangeLength * 1.13);
-					tolerance = 25;
-					indexOfCharacter = scaledIndexOfCharacter;
-				}
-
-				if (label.Font.Name.StartsWith("HelveticaNeue", StringComparison.InvariantCulture))
-				{
-					if (link.Range.Location > 2000)
-					{
-						indexOfCharacter = scaledIndexOfCharacter;
-					}
-				}
-
-				// Xamarin version of NSLocationInRange?
-				if ((indexOfCharacter >= (link.Range.Location - tolerance)) && (indexOfCharacter < (link.Range.Location + rangeLength + tolerance)))
-				{
-					return link.Url;
-				}
-			}
-			return null;
-		}
-
-		private class LinkData
-		{
-			public LinkData(NSRange range, string url) { Range = range; Url = url; }
-			public readonly NSRange Range;
-			public readonly string Url;
+			var element = (HtmlLabel)Element;
+			control.AttributedText = mutableHtmlString;
+			control.HandleLinkTap(element);
 		}
 	}
 }
